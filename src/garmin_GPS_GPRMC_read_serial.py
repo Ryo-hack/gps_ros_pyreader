fd7c3a34a136d0dfe4be837b15266b62dcd198b8
Ryo-hack <r114174@g.kobe-kosen.ac.jp>
2022-05-05 03:17:56 +0900

    sec commit

--- src/garmin_GPS_GPRMC_read_serial.py
+++ src/garmin_GPS_GPRMC_read_serial.py
@@ -10,6 +10,7 @@ from std_msgs.msg import Float32
 from sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import  NavSatStatus
 
+
 class GPS :
     def __init__(self):
         self.garmin_GPS_data  = 0.0
@@ -20,31 +21,14 @@ class GPS :
         self.Longitude = 0.0
         self.altitude = 0.0
 
-    def set_serial(self):
+    def serial_read(self):
         port_name = rospy.get_param('~port','/dev/ttyUSB0')
         if len(sys.argv) == 2 :
             port_name  = sys.argv[1]
         self.ser = serial.Serial(port_name,9600,timeout=0.1)
         #rospy.loginfo("Connected on %s" % (port_name) )
         time.sleep(1)
-        return self.ser
-
-    def serial_read(self):
-        #self.ser = serial.Serial('/dev/ttyUSB0',9600,timeout=0.1)
         self.garmin_GPS_data = self.ser.readline()
-        if len(self.garmin_GPS_data) == 74 :
-            self.data_adjust()
-            rospy.loginfo(self.garmin_GPS_data)
-            self.publisher()
-
-        elif len(self.garmin_GPS_data) == 0 :
-            rospy.loginfo(self.garmin_GPS_data)
-            rospy.loginfo("GPS LOST")
-
-        else :
-            rospy.loginfo(self.garmin_GPS_data)
-            rospy.loginfo("Data corruption")
-
         self.ser.close()
         return self.GPS_data
 
@@ -64,11 +48,10 @@ class GPS :
         self.Longitude = Longitude + Longitude_dec
 
         self.altitude = 0.0
-        rospy.loginfo(data)
+
 
 
     def publisher(self):
-        self.serial_read()
         Fix = NavSatStatus()
         #Fix.status = GPS.mode
         #Fix.service = GPS.numSat
@@ -86,8 +69,20 @@ if __name__ == '__main__':
     rospy.init_node("garmin_GPS_talker")
     GPS = GPS()
     
-    rate = rospy.Rate(1)
+    rate = rospy.Rate(1000)
     while not rospy.is_shutdown():
-        GPS.set_serial()
         GPS.serial_read()
+        if len(GPS.garmin_GPS_data) == 74 :
+            GPS.data_adjust()
+            rospy.loginfo(GPS.garmin_GPS_data)
+            GPS.publisher()
+            
+        elif len(GPS.garmin_GPS_data) == 0 :
+            rospy.loginfo(GPS.garmin_GPS_data)
+            rospy.loginfo("GPS LOST")
+
+        else :
+            rospy.loginfo(GPS.garmin_GPS_data)
+            rospy.loginfo("Data corruption")
+
         rate.sleep()
